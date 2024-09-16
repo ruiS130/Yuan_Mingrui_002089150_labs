d0938bef902bcc345763b03f03926bf64f84bc2f
ruiS130 <yuan.ming@northeastern.edu>
2024-09-16 15:27:13 -0400

    Initial commit

---
+++ NetBeansProjects/Assignment_1/src/Model/Person.java
@@ -0,0 +1,140 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author ruirui
+ */
+public class Person {
+
+    String FirstName;
+    String LastName;
+    String Email;
+    String Phone;
+    String dLicenseNum;
+    String dLicenseStete;
+    String socialSNum;
+    String addressL1;
+    String AddressL2;
+    String City;
+    String State;
+    String ZIP;
+    String teleNum;
+    String faxNum;
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String Phone) {
+        this.Phone = Phone;
+    }
+
+    public String getdLicenseNum() {
+        return dLicenseNum;
+    }
+
+    public void setdLicenseNum(String dLicenseNum) {
+        this.dLicenseNum = dLicenseNum;
+    }
+
+    public String getdLicenseStete() {
+        return dLicenseStete;
+    }
+
+    public void setdLicenseStete(String dLicenseStete) {
+        this.dLicenseStete = dLicenseStete;
+    }
+
+    public String getSocialSNum() {
+        return socialSNum;
+    }
+
+    public void setSocialSNum(String socialSNum) {
+        this.socialSNum = socialSNum;
+    }
+
+    public String getAddressL1() {
+        return addressL1;
+    }
+
+    public void setAddressL1(String addressL1) {
+        this.addressL1 = addressL1;
+    }
+
+    public String getAddressL2() {
+        return AddressL2;
+    }
+
+    public void setAddressL2(String AddressL2) {
+        this.AddressL2 = AddressL2;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String State) {
+        this.State = State;
+    }
+
+    public String getZIP() {
+        return ZIP;
+    }
+
+    public void setZIP(String ZIP) {
+        this.ZIP = ZIP;
+    }
+
+    public String getTeleNum() {
+        return teleNum;
+    }
+
+    public void setTeleNum(String teleNum) {
+        this.teleNum = teleNum;
+    }
+
+    public String getFaxNum() {
+        return faxNum;
+    }
+
+    public void setFaxNum(String faxNum) {
+        this.faxNum = faxNum;
+    }
+    
+}
