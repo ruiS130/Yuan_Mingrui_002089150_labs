c5d4812c04e2d58e98761ba17ad52b41de15f3c7
ruiS130 <yuan.ming@northeastern.edu>
2024-09-18 21:00:16 -0400

    Finish assignment: Complete implementation and testing

--- NetBeansProjects/Assignment_1/src/Model/Person.java
+++ NetBeansProjects/Assignment_1/src/Model/Person.java
@@ -15,15 +15,40 @@ public class Person {
     String Email;
     String Phone;
     String dLicenseNum;
-    String dLicenseStete;
+    String dLicenseState;
     String socialSNum;
-    String addressL1;
-    String AddressL2;
     String City;
-    String State;
     String ZIP;
     String teleNum;
     String faxNum;
+    String sex;
+    String age;
+    
+    String mStat;
+    String language;
+    String perWeb;
+    String conOrigin;
+    
+    String wAddress1;
+    String wAddress2;
+    String wCity;
+    String wState;
+    String wZip;
+    String wPhone;
+    
+    String locAddress1;
+    String locAddress2;
+    String locCity;
+    String locState;
+    String locZip;
+    String locPhone;
+    
+    String hAddress1;
+    String hAddress2;
+    String hCity;
+    String hState;
+    String hZip;
+    String hPhone;
 
     public String getFirstName() {
         return FirstName;
@@ -65,12 +90,12 @@ public class Person {
         this.dLicenseNum = dLicenseNum;
     }
 
-    public String getdLicenseStete() {
-        return dLicenseStete;
+    public String getdLicenseState() {
+        return dLicenseState;
     }
 
-    public void setdLicenseStete(String dLicenseStete) {
-        this.dLicenseStete = dLicenseStete;
+    public void setdLicenseState(String dLicenseState) {
+        this.dLicenseState = dLicenseState;
     }
 
     public String getSocialSNum() {
@@ -81,36 +106,12 @@ public class Person {
         this.socialSNum = socialSNum;
     }
 
-    public String getAddressL1() {
-        return addressL1;
-    }
-
-    public void setAddressL1(String addressL1) {
-        this.addressL1 = addressL1;
-    }
-
-    public String getAddressL2() {
-        return AddressL2;
-    }
-
-    public void setAddressL2(String AddressL2) {
-        this.AddressL2 = AddressL2;
-    }
-
     public String getCity() {
         return City;
     }
 
     public void setCity(String City) {
         this.City = City;
-    }
-
-    public String getState() {
-        return State;
-    }
-
-    public void setState(String State) {
-        this.State = State;
     }
 
     public String getZIP() {
@@ -135,6 +136,198 @@ public class Person {
 
     public void setFaxNum(String faxNum) {
         this.faxNum = faxNum;
+    }
+
+    public String getmStat() {
+        return mStat;
+    }
+
+    public void setmStat(String mStat) {
+        this.mStat = mStat;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getPerWeb() {
+        return perWeb;
+    }
+
+    public void setPerWeb(String perWeb) {
+        this.perWeb = perWeb;
+    }
+
+    public String getConOrigin() {
+        return conOrigin;
+    }
+
+    public void setConOrigin(String conOrigin) {
+        this.conOrigin = conOrigin;
+    }
+
+    public String getwAddress1() {
+        return wAddress1;
+    }
+
+    public void setwAddress1(String wAddress1) {
+        this.wAddress1 = wAddress1;
+    }
+
+    public String getwAddress2() {
+        return wAddress2;
+    }
+
+    public void setwAddress2(String wAddress2) {
+        this.wAddress2 = wAddress2;
+    }
+
+    public String getwCity() {
+        return wCity;
+    }
+
+    public void setwCity(String wCity) {
+        this.wCity = wCity;
+    }
+
+    public String getwState() {
+        return wState;
+    }
+
+    public void setwState(String wState) {
+        this.wState = wState;
+    }
+
+    public String getwZip() {
+        return wZip;
+    }
+
+    public void setwZip(String wZip) {
+        this.wZip = wZip;
+    }
+
+    public String getwPhone() {
+        return wPhone;
+    }
+
+    public void setwPhone(String wPhone) {
+        this.wPhone = wPhone;
+    }
+
+    public String getLocAddress1() {
+        return locAddress1;
+    }
+
+    public void setLocAddress1(String locAddress1) {
+        this.locAddress1 = locAddress1;
+    }
+
+    public String getLocAddress2() {
+        return locAddress2;
+    }
+
+    public void setLocAddress2(String locAddress2) {
+        this.locAddress2 = locAddress2;
+    }
+
+    public String getLocCity() {
+        return locCity;
+    }
+
+    public void setLocCity(String locCity) {
+        this.locCity = locCity;
+    }
+
+    public String getLocState() {
+        return locState;
+    }
+
+    public void setLocState(String locState) {
+        this.locState = locState;
+    }
+
+    public String getLocZip() {
+        return locZip;
+    }
+
+    public void setLocZip(String locZip) {
+        this.locZip = locZip;
+    }
+
+    public String getLocPhone() {
+        return locPhone;
+    }
+
+    public void setLocPhone(String locPhone) {
+        this.locPhone = locPhone;
+    }
+
+    public String gethAddress1() {
+        return hAddress1;
+    }
+
+    public void sethAddress1(String hAddress1) {
+        this.hAddress1 = hAddress1;
+    }
+
+    public String gethAddress2() {
+        return hAddress2;
+    }
+
+    public void sethAddress2(String hAddress2) {
+        this.hAddress2 = hAddress2;
+    }
+
+    public String gethCity() {
+        return hCity;
+    }
+
+    public void sethCity(String hCity) {
+        this.hCity = hCity;
+    }
+
+    public String gethState() {
+        return hState;
+    }
+
+    public void sethState(String hState) {
+        this.hState = hState;
+    }
+
+    public String gethZip() {
+        return hZip;
+    }
+
+    public void sethZip(String hZip) {
+        this.hZip = hZip;
+    }
+
+    public String gethPhone() {
+        return hPhone;
+    }
+
+    public void sethPhone(String hPhone) {
+        this.hPhone = hPhone;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
     }
     
 }
