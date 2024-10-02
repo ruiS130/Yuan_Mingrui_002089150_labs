e309790768016ff95deb20a7f668853cd78018bf
Mingrui <yuan.ming@northeastern.edu>
2024-10-02 15:15:05 -0400

    Update to Model and UI packages

---
+++ NetBeansProjects/Assignment_2/src/Model/Person.java
@@ -0,0 +1,278 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author ruirui
+ */
+public class Person {
+
+    String FirstName;
+    String LastName;
+    String Email;
+    String Phone;
+    String dLicenseNum;
+    String dLicenseState;
+    String socialSNum;
+    String City;
+    String ZIP;
+    String teleNum;
+    String faxNum;
+    String sex;
+    String age;
+    
+    String mStat;
+    String language;
+    String perWeb;
+    String conOrigin;
+    
+    String wAddress1;
+    String wAddress2;
+    String wCity;
+    String wState;
+    String wZip;
+    String wPhone;
+    
+    String hAddress1;
+    String hAddress2;
+    String hCity;
+    String hState;
+    String hZip;
+    String hPhone;
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String Phone) {
+        this.Phone = Phone;
+    }
+
+    public String getdLicenseNum() {
+        return dLicenseNum;
+    }
+
+    public void setdLicenseNum(String dLicenseNum) {
+        this.dLicenseNum = dLicenseNum;
+    }
+
+    public String getdLicenseState() {
+        return dLicenseState;
+    }
+
+    public void setdLicenseState(String dLicenseState) {
+        this.dLicenseState = dLicenseState;
+    }
+
+    public String getSocialSNum() {
+        return socialSNum;
+    }
+
+    public void setSocialSNum(String socialSNum) {
+        this.socialSNum = socialSNum;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getZIP() {
+        return ZIP;
+    }
+
+    public void setZIP(String ZIP) {
+        this.ZIP = ZIP;
+    }
+
+    public String getTeleNum() {
+        return teleNum;
+    }
+
+    public void setTeleNum(String teleNum) {
+        this.teleNum = teleNum;
+    }
+
+    public String getFaxNum() {
+        return faxNum;
+    }
+
+    public void setFaxNum(String faxNum) {
+        this.faxNum = faxNum;
+    }
+
+    public String getmStat() {
+        return mStat;
+    }
+
+    public void setmStat(String mStat) {
+        this.mStat = mStat;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getPerWeb() {
+        return perWeb;
+    }
+
+    public void setPerWeb(String perWeb) {
+        this.perWeb = perWeb;
+    }
+
+    public String getConOrigin() {
+        return conOrigin;
+    }
+
+    public void setConOrigin(String conOrigin) {
+        this.conOrigin = conOrigin;
+    }
+
+    public String getwAddress1() {
+        return wAddress1;
+    }
+
+    public void setwAddress1(String wAddress1) {
+        this.wAddress1 = wAddress1;
+    }
+
+    public String getwAddress2() {
+        return wAddress2;
+    }
+
+    public void setwAddress2(String wAddress2) {
+        this.wAddress2 = wAddress2;
+    }
+
+    public String getwCity() {
+        return wCity;
+    }
+
+    public void setwCity(String wCity) {
+        this.wCity = wCity;
+    }
+
+    public String getwState() {
+        return wState;
+    }
+
+    public void setwState(String wState) {
+        this.wState = wState;
+    }
+
+    public String getwZip() {
+        return wZip;
+    }
+
+    public void setwZip(String wZip) {
+        this.wZip = wZip;
+    }
+
+    public String getwPhone() {
+        return wPhone;
+    }
+
+    public void setwPhone(String wPhone) {
+        this.wPhone = wPhone;
+    }
+
+    public String gethAddress1() {
+        return hAddress1;
+    }
+
+    public void sethAddress1(String hAddress1) {
+        this.hAddress1 = hAddress1;
+    }
+
+    public String gethAddress2() {
+        return hAddress2;
+    }
+
+    public void sethAddress2(String hAddress2) {
+        this.hAddress2 = hAddress2;
+    }
+
+    public String gethCity() {
+        return hCity;
+    }
+
+    public void sethCity(String hCity) {
+        this.hCity = hCity;
+    }
+
+    public String gethState() {
+        return hState;
+    }
+
+    public void sethState(String hState) {
+        this.hState = hState;
+    }
+
+    public String gethZip() {
+        return hZip;
+    }
+
+    public void sethZip(String hZip) {
+        this.hZip = hZip;
+    }
+
+    public String gethPhone() {
+        return hPhone;
+    }
+
+    public void sethPhone(String hPhone) {
+        this.hPhone = hPhone;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+    
+}
